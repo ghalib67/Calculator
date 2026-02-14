cd5cf50abd7878fae0a06e915b72ff16c5b99688
ghalib67 <ghalibkaka1234@gmail.com>
2026-02-14 19:22:23 +0500

    done

--- script.js
+++ script.js
@@ -32,18 +32,55 @@ function operate(operator, a, b){
 
 const display = document.querySelector(".display")
 const numberBtns = document.querySelectorAll("button")
+let operator = ""
 
 numberBtns.forEach(button =>{
     button.addEventListener("click", () =>{
-        if (button.dataset.value === "equals"){
-            let operator = display.textContent.match(/[+\-*\/]/)[0];
+        if (display.textContent === "Cannot divide by 0"){
+            display.textContent = ""
+            operator = ""
+        } 
+
+        if (button.dataset.value === "equals" && !button.classList.contains("operator")){
+            let op = display.textContent.match(/[+\-*\/]/)[0];
             let parts = display.textContent.split(/\+|\-|\*|\//)
-            display.textContent = operate(operator,Number(parts[0]),Number(parts[1]))
+            if (op === "/" && Number(parts[1]) === 0) {
+                display.textContent = "Cannot divide by 0";
+                return;
+            }
+            display.textContent = Math.round(operate(op,Number(parts[0]),Number(parts[1])))
             return
         }
+
         else if(button.dataset.value === "clear"){
             display.textContent = ""
         }
+
+        else if (button.classList.contains("operator")){
+
+            if (operator === ""){
+
+                operator = button.dataset.value
+                display.textContent += button.dataset.value
+            } 
+
+            else{
+
+            let op = display.textContent.match(/[+\-*\/]/)[0];
+            let parts = display.textContent.split(/\+|\-|\*|\//)
+            if (op === "/" && Number(parts[1]) === 0) {
+                display.textContent = "Cannot divide by 0";
+                return;
+            }
+            display.textContent = Math.round(operate(op,Number(parts[0]),Number(parts[1])))
+            operator = button.dataset.value
+            display.textContent += button.dataset.value
+            return
+
+            }
+            
+        }
+            
         else{
 
             display.textContent += button.dataset.value
